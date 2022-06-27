7aba167b9bdc806c74593abc2417c5915201c67e
Donmikko Dan M. Olmillo <57443799+mikkolmillo@users.noreply.github.com>
2022-06-27 11:31:54 +0800

    add credit card design

--- components/Layout/index.js
+++ components/Layout/index.js
@@ -1,22 +1,14 @@
 import React from "react";
-import { Button } from "@material-ui/core";
 import Header from "../header";
 import styles from "../../styles/Home.module.css";
 import classes from "./index.module.css";
+import { SiEthereum } from 'react-icons/si'
+import { BsInfoCircle } from 'react-icons/bs'
 import { useTranslation } from "next-i18next";
+import { shortenAddress } from '../../utils/shortenAddress'
 
 export default function Layout({ changeTheme, theme, children }) {
   const { t } = useTranslation("common");
-  // const addNetwork = () => {
-  //   window.open("https://github.com/ethereum-lists/chains", "_blank");
-  // };
-
-  // const addRpc = () => {
-  //   window.open(
-  //     "https://github.com/DefiLlama/chainlist/blob/main/constants/extraRpcs.json",
-  //     "_blank"
-  //   );
-  // };
 
   // const url = '/logo.svg'
 
@@ -36,6 +28,24 @@ export default function Layout({ changeTheme, theme, children }) {
                 width: '360px',
                 marginLeft: '-15px',
               }} src={url} alt="Chainlist logo" /> */}
+              <div className={`p-3 mx-auto rounded-xl h-40 lg:h-52 w-72 sm:w-full my-5 ${classes['white-glassmorphism']} ${classes['eth-card']}`}>
+                <div className="flex justify-between flex-col w-full h-full">
+                  <div className="flex justify-between items-start">
+                    <div className="w-10 h-10 rounded-full border-2 border-white flex justify-center items-center">
+                      <SiEthereum fontSize={21} color='#fff' />
+                    </div>
+                    <BsInfoCircle fontSize={17} color='#fff' />
+                  </div>
+                  <div>
+                    <p className="text-white font-light text-sm">
+                      {shortenAddress('0xD8C42316e2bAFa294C25fc8852dD4935a18511B4')}
+                    </p>
+                    <p className="text-white font-semibold text-lg mt-1">
+                      Ethereum
+                    </p>
+                  </div>
+                </div>
+              </div>
 
               <h2 className="my-10 font-bold text-2xl">
                 {/* {t("help-info")} */}
@@ -45,30 +55,6 @@ export default function Layout({ changeTheme, theme, children }) {
                 Lorem ipsum dolor sit amet consectetur adipisicing elit. Asperiores sint delectus molestiae, sequi corporis minima odio harum a laborum? Perferendis, tenetur illo provident sequi error quam sed iste debitis nam? Voluptatx.
                 {/* {t("description")} */}
               </p>
-              {/* <Button
-                size="large"
-                color="primary"
-                variant="contained"
-                className={classes.addNetworkButton}
-                onClick={addNetwork}
-                endIcon={<AddIcon />}
-              >
-                <Typography className={classes.buttonLabel}>
-                  {t("add-your-network")}
-                </Typography>
-              </Button>
-              <Button
-                size="large"
-                color="primary"
-                variant="outlined"
-                className={classes.addRpcButton}
-                onClick={addRpc}
-                endIcon={<AddIcon />}
-              >
-                <Typography className={classes.buttonLabel}>
-                  {t("add-your-rpc")}
-                </Typography>
-              </Button> */}
             </div>
           </div>
           <div
