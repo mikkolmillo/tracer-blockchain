ef2dd6172c95cacaec400ce15beb30d1b791d10a
Donmikko Dan M. Olmillo <57443799+mikkolmillo@users.noreply.github.com>
2022-07-01 11:22:14 +0800

    add deployed contract and abi to the frontend

--- utils/constants.js
+++ utils/constants.js
@@ -1,6 +1,10 @@
-import abi from './abi/Transactions.json'
+import abi from './abi/MultiSend.json'
 
 export const contractAbi = abi.abi
 // Paste smart contract after being deployed
 // here:
-export const contractAddress = '0xc09F8860467C526647Adb1a3232EC5F716b0dbb4'
+export const contractAddress = '0x8f4533C096Ecdfd631204FD24f392Eb5Ee5AE5f1'
+
+// Paste the created abi from
+// artifacts/contracts/[:DeployedContract]
+// to the /abi folder
