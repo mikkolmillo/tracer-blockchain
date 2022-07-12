51a7510ffa63f7dbb764f2e549f279fb999e2f41
Donmikko Dan M. Olmillo <57443799+mikkolmillo@users.noreply.github.com>
2022-07-12 15:12:54 +0800

    deploy contract to polygon mumbai testnet

--- contracts/hardhat.config.js
+++ contracts/hardhat.config.js
@@ -28,6 +28,14 @@ module.exports = {
       accounts: ['6e260defa4c747bbcb58c2a70be15c3501c7f9d73683e13ebb9296933f66fd30'],
       gasPrice: 8000000000, // default is 'auto' which breaks chains without the london hardfork
     },
+    matic_testnet: {
+      // url: 'https://eth-ropsten.alchemyapi.io/v2/WezfmiT2fd1KObLKfLX3BBbzgeI6Xy9y',
+      url: 'https://polygon-mumbai.g.alchemy.com/v2/CaEHub3mGCnYcEIdE1F7SoEPYhyuynC0',
+      // ! DEV ENV 1 Private Key
+      // * Address use to find the contract
+      accounts: ['6e260defa4c747bbcb58c2a70be15c3501c7f9d73683e13ebb9296933f66fd30'],
+      gasPrice: 35000000000, // default is 'auto' which breaks chains without the london hardfork
+    },
   },
   paths: {
     sources: "./contracts",
