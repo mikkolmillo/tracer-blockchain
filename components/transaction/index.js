bcd5b2b8068eca1b1762abe73f7a539bd5e9cf8e
Donmikko Dan M. Olmillo <57443799+mikkolmillo@users.noreply.github.com>
2022-07-13 10:09:19 +0800

    add more rpc urls on binance main nets and test nets

--- components/transaction/index.js
+++ components/transaction/index.js
@@ -36,7 +36,19 @@ const mainnets = {
       symbol: 'BNB',
       decimals: 18
     },
-    rpcUrls: ['https://bsc-dataseed.binance.org/'],
+    rpcUrls: [
+      'https://bsc-dataseed1.binance.org/',
+      'https://bsc-dataseed2.binance.org/',
+      'https://bsc-dataseed3.binance.org/',
+      'https://bsc-dataseed4.binance.org/',
+      'https://bsc-dataseed1.defibit.io/',
+      'https://bsc-dataseed3.defibit.io/',
+      'https://bsc-dataseed4.defibit.io/',
+      'https://bsc-dataseed1.ninicoin.io/',
+      'https://bsc-dataseed2.ninicoin.io/',
+      'https://bsc-dataseed3.ninicoin.io/',
+      'https://bsc-dataseed4.ninicoin.io/',
+    ],
     blockExplorerUrls: ['https://bscscan.com']
   },
   fantom: {
@@ -105,7 +117,14 @@ const testnets = {
       symbol: 'BNB',
       decimals: 18
     },
-    rpcUrls: ['https://data-seed-prebsc-1-s1.binance.org:8545'],
+    rpcUrls: [
+      'https://data-seed-prebsc-1-s1.binance.org:8545/',
+      'https://data-seed-prebsc-2-s1.binance.org:8545/',
+      'http://data-seed-prebsc-1-s2.binance.org:8545/',
+      'http://data-seed-prebsc-2-s2.binance.org:8545/',
+      'https://data-seed-prebsc-1-s3.binance.org:8545/',
+      'https://data-seed-prebsc-2-s3.binance.org:8545/',
+    ],
     blockExplorerUrls: ['https://testnet.bscscan.com/']
   },
   fantom: {
